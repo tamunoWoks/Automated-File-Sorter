c7e80411949ecb7ebb012369ad217a0bfd630d1d
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-10-29 20:40:17 -0600

    Define file extensions and their corresponding folders

--- sorter.py
+++ sorter.py
@@ -3,3 +3,10 @@ import os, shutil, time
 
 # Define the path
 path = r"C:/Users/ADMIN/Documents/Python/File sorter/"
+
+# Define file extensions and their corresponding folders
+folder_mappings = {
+    '.csv': 'csv files',
+    '.png': 'image files',
+    '.txt': 'text files'
+}
